528d60044b819b81711db1cf29ab542e41475d7d
manatran <manatran@student.arteveldehs.be>
2018-03-25 18:06:01 +0200

    Deploy to heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api', apiRoutes);
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.send('GDM GENT API')
 });
 app.use((req, res, next) => {
   const err = new Error('Not Found');
